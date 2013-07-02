b82582443d9aea16ed94cf08ff4e1cfc4b727380
Ken Arnold <arnold@peak.im>
2013-07-01 23:20:42 -0400

    Make it one large project

--- compare_to_default.sh
+++ compare_to_default.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-diff -r default_resourcepack clarity | egrep -v '^Binary|texture.item: |: \.|clarity: pack'
+diff -r default_resourcepack clarity | egrep -v '^Binary|textures/items: |: \.|clarity: pack'
