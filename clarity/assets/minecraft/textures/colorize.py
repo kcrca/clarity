7cbc5c80de73e8711a7e83f18fd5544608dcbfed
Ken Arnold <arnold@peak.im>
2013-07-02 13:19:09 -0400

    Colorize ores

--- clarity/assets/minecraft/textures/colorize.py
+++ clarity/assets/minecraft/textures/colorize.py
@@ -1,3 +1,6 @@
+import os
+import sys
+import getopt
 import ConfigParser
 import re
 import Image
@@ -7,33 +10,33 @@ __author__ = 'arnold'
 config = ConfigParser.SafeConfigParser()
 config.read('colorize.cfg')
 
-color_pat = re.compile(r'\((\d+),(\d+),(\d+)\)')
-
-colorings = config.items('colorings')
-maps = {}
+color_re = re.compile(r'\(?(\d+),(\d+),(\d+)\)?')
 
 
-def color_list(color_name, colors_config):
+def decode_color(color):
+    m = color_re.match(color)
+    if not m:
+        print('Bad color: %s' % color)
+    else:
+        return tuple(map(int, m.groups()))
+
+
+def color_list(map_name, color_name, colors_config):
     l = []
     colors = colors_config.split()
     for color in colors:
-        m = color_pat.match(color)
-        if not m:
-            print('Bad color: %s: %s: %s' % (map_name, color_name, color))
-        else:
-            r, g, b = map(int, m.groups())
-            l.append((r, g, b))
+        l.append(decode_color(color))
     return l
 
 
 def map_for(map_name, key_color):
-    key_list = color_list(key_color, config.get(map_name, key_color))
+    key_list = color_list(map_name, key_color, config.get(map_name, key_color))
     num_colors = len(key_list)
     color_map = {}
     for color_name, colors_config in config.items(map_name):
         if color_name == key_color:
             continue
-        l = color_list(color_name, colors_config)
+        l = color_list(map_name, color_name, colors_config)
         if len(l) != num_colors:
             print("Mismatch: %s: %s: expected %d colors, found %d" % (
                 map_name, color_name, num_colors, len(l)))
@@ -45,22 +48,109 @@ def map_for(map_name, key_color):
     return color_map
 
 
-for coloring, coloring_config in colorings:
-    map_name, key_color, file_pat = coloring_config.split()
-    color_maps = map_for(map_name, key_color)
-    key_file = file_pat.replace('COLOR', key_color)
-    src_img = Image.open(key_file)
-    src_data = src_img.load()
-    dst_img = Image.new('RGB', src_img.size, color=None)
-    dst_data = dst_img.load()
-    color_maps = map_for(map_name, key_color)
-    for color_name, color_map in color_maps.iteritems():
-        for x in xrange(src_img.size[0]):
-            for y in xrange(src_img.size[1]):
-                r, g, b, a = src_data[x, y]
-                try:
-                    r, g, b = color_map[(r, g, b)]
-                except KeyError:
-                    pass
-                dst_data[x, y] = r, g, b
-        dst_img.save(file_pat.replace('COLOR', color_name), "png")
+class Usage(Exception):
+    def __init__(self, msg):
+        self.msg = msg
+
+
+def decode_coloring(coloring):
+    coloring_config = config.get('colorings', coloring)
+    return coloring_config.split()
+
+
+def list_colors(color_name, file_name, exclude_colors):
+    img = Image.open(file_name).convert("RGB")
+    data = img.load()
+    colors = set()
+    for x in xrange(img.size[0]):
+        for y in xrange(img.size[1]):
+            r, g, b = data[x, y]
+            colors.add((r, g, b))
+    sys.stdout.write("%-13s" % (color_name + ':'))
+    for c in sorted(colors, cmp=lambda x, y: sum(x) - sum(y), reverse=True):
+        if not c in exclude_colors:
+            # noinspection PyStringFormat
+            sys.stdout.write(" %-13s" % ("(%d,%d,%d)" % c[:]))
+    print ""
+
+
+def file_from_color(file_pat, color_name):
+    return file_pat.replace('COLOR', color_name)
+
+
+def list_coloring(coloring, exclude_colors):
+    map_name, key_color, file_pat = decode_coloring(coloring)
+    key_file = file_from_color(file_pat, key_color)
+    print "[%s]" % coloring
+    print ""
+    list_colors(key_color, key_file, exclude_colors)
+    file_re = re.compile(file_from_color(os.path.basename(file_pat), r'(.*)'))
+    file_dir = os.path.dirname(file_pat)
+    for f in sorted(os.listdir(file_dir)):
+        m = file_re.match(f)
+        if m:
+            color_name = m.groups()[0]
+            list_colors(color_name, '%s/%s' % (file_dir, f), exclude_colors)
+
+
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv
+    try:
+        try:
+            opts, args = getopt.getopt(argv[1:], "hl:x:",
+                                       ["help", "list", "exclude"])
+        except getopt.error, msg:
+            raise Usage(msg)
+            # more code, unchanged
+    except Usage, err:
+        print >> sys.stderr, err.msg
+        print >> sys.stderr, "for help use --help"
+        return 2
+
+    list_colorings = []
+    exclude_colors = set()
+    # process options
+    for o, a in opts:
+        if o in ("-h", "--help"):
+            print __doc__
+            return 0
+        if o in ("-l", "--list"):
+            list_colorings.append(a)
+        elif o in ("-x", "--exclude"):
+            color = decode_color(a)
+            exclude_colors.add(color)
+
+    if len(list_colorings):
+        for coloring in list_colorings:
+            list_coloring(coloring, exclude_colors)
+        return 0
+
+    colorings = config.items('colorings')
+    for coloring, coloring_config in colorings:
+        map_name, key_color, file_pat = decode_coloring(coloring)
+        key_file = file_from_color(file_pat, key_color)
+        print "%s: reading %s" % (coloring, key_file)
+        src_img = Image.open(key_file)
+        src_data = src_img.load()
+
+        color_maps = map_for(map_name, key_color)
+        for color_name, color_map in color_maps.iteritems():
+            dst_file = file_from_color(file_pat, color_name)
+            print ("    %s" % dst_file)
+            dst_img = Image.new('RGB', src_img.size, color=None)
+            dst_data = dst_img.load()
+            for x in xrange(src_img.size[0]):
+                for y in xrange(src_img.size[1]):
+                    r, g, b = src_data[x, y][:3]
+                    try:
+                        r, g, b = color_map[(r, g, b)]
+                    except KeyError:
+                        pass
+                    dst_data[x, y] = r, g, b
+            dst_img.save(dst_file, "png")
+
+
+if __name__ == "__main__":
+    sys.exit(main())
+
