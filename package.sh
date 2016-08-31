bed6df13ccdbf804e5d3ce777326bff46143baa0
Ken Arnold <arnold@peak.im>
2016-08-30 20:45:14 -0700

    Mack package names uppercase

--- package.sh
+++ package.sh
@@ -16,8 +16,7 @@ rm -rf packs $dirs
 test -d packs || mkdir packs
 packs=$PWD/packs
 out=$packs/repack.out
-rm -f $out
-touch $out
+cp /dev/null $out
 
 echo Regenerating derived files in core
 for f in `find . -name '*.py'`; do
@@ -36,50 +35,60 @@ for f in `find . -name '*.py'`; do
     esac
 done
 
+function to_title() {
+    echo "$(tr a-z A-Z <<< ${1:0:1})${1:1}"
+}
+
 # This function will build a single zip file
 function do_zip() {
     (
-	cd $1 
-	rm -f $packs/$1.zip
+	name=$1
+	ucname=`to_title $name`
+	echo $name - $ucname
+	cd $name
+	rm -f $packs/$ucname.zip
 
-	echo Building $1.zip
+	echo Building $ucname.zip
 	# The "-o" flag says to make the mod time on the zip file the same
 	# as the most recent mod time on any of the files. This lets the
 	# zip file's mod time stand for the most recent change in the entire
 	# zipped directory.
-	zip -rDqoy $packs/$1.zip *
+	zip -rDqoy $packs/$ucname.zip *
     )
 }
 
 # Creates special subparts of the texture pack set
 function do_create() {
     name=$1
+    ucname=`to_title $name`
     shift
-    echo ... Creating $name
-    mkdir -p $name
-    tar c -C clarity "$@" | tar xf - -C $name
+    echo ... Creating $ucname
+    mkdir -p $ucname
+    tar c -C clarity "$@" | tar xf - -C $ucname
     # Trivial implementation of repack for this case
-    tar c -C $name.repack/override . | tar xf - -C $name
-    find $name -name '*.pxm' | xargs rm
+    tar c -C $name.repack/override . | tar xf - -C $ucname
+    find $name -name '*.pxm' -print0 | xargs -0 rm
 }
 
 echo ... Repacking
 python repack/repack.py $f >> $out || ( cat $out ; exit 1)
 
 rm -f home
+# Works for a mac, should check for other configurations
 ln -s $HOME/Library/Application\ Support/minecraft home
-for f in "${dirs[@]}"; do
-    case "$f" in
+for name in "${dirs[@]}"; do
+    ucname=`to_title $name`
+    case "$name" in
       "beguile")
-	do_create $f assets/minecraft/textures/gui assets/minecraft/textures/font
+	do_create $name assets/minecraft/textures/gui assets/minecraft/textures/font
 	;;
       *)
 	;;
     esac
-    do_zip $f
+    do_zip $name
     (
 	cd home/resourcepacks
-	rm -f $f $f.zip
-	ln -s $packs/../$f .
+	rm -f $ucname $ucname.zip $name $name.zip
+	ln -s $packs/../$ucname .
     )
 done
