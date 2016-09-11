da42ab0e502416bfae46e085149568c1d61a2acc
Ken Arnold <arnold@peak.im>
2016-09-11 12:23:44 -0700

    Misc stuff

--- core/assets/minecraft/report.py
+++ core/assets/minecraft/report.py
@@ -1,20 +1,49 @@
 #!/usr/bin/env python
+import os
 import re
 import subprocess
 import ConfigParser
 import sys
+
+import Image
+import ImageChops
 
 __author__ = 'arnold'
 
 status_by_name = {}
 
 
+def no_path(groups):
+    return None
+
+
+def whole_match(groups):
+    assert len(groups) == 1
+    return groups[0]
+
+
+def first_group(groups):
+    assert len(groups) == 2
+    return groups[1]
+
+
+def path_from_only(groups):
+    path = None
+    assert len(groups) == 2
+    if len(groups[0]) > 0:
+        path = os.path.join(groups[0], groups[1])
+    else:
+        path = groups[1]
+    return path
+
+
 class FileStatus(object):
-    def __init__(self, prefix, pattern, ignore_pats=()):
+    def __init__(self, prefix, pattern, ignore_pats=(), path_from_groups=whole_match):
         self.pat = re.compile(pattern)
         self.prefix = prefix
         self.files = set()
         self.ignore = []
+        self.path_from_groups = path_from_groups
         try:
             to_ignore = config.get('ignore', prefix.lower())
             all_ignored = ignore_pats + tuple(to_ignore.split())
@@ -27,21 +56,35 @@ class FileStatus(object):
         m = self.pat.search(line)
         if m:
             groups = m.groups()
-            path = None
-            if len(groups) == 1:
-                path = groups[0]
-            elif len(groups) == 2:
-                if len(groups[0]) > 0:
-                    path = groups[0] + '/' + groups[1]
-                else:
-                    path = groups[1]
+            path = self.path_from_groups(groups) if len(groups) else None
             if path and not any(pat.search(path) for pat in self.ignore):
-                self.files.add(path)
+                self.add_path(groups, path)
         return m
 
     def dump(self):
         for f in sorted(self.files):
             print '%s: %s' % (self.prefix, f)
+
+    def add_path(self, groups, path):
+        self.files.add(path)
+
+
+# This does an extra check for changed PNG files to see if they are really different
+class ChangedFileStatus(FileStatus):
+    def __init__(self, prefix, pattern, same_file_status, ignore_pats=()):
+        super(ChangedFileStatus, self).__init__(prefix, pattern, ignore_pats, first_group)
+        self.same_file_status = same_file_status
+
+    def add_path(self, groups, path):
+        if path.endswith('.png'):
+            # Check if they are equivalent images, just encoded differently.
+            img1 = Image.open(groups[0])
+            img2 = Image.open(groups[1])
+            if img1.size == img2.size and ImageChops.difference(img1.convert('RGBA'),
+                                                                img2.convert('RGBA')).getbbox() is None:
+                self.same_file_status.add_path(groups, path)
+                return
+        self.files.add(path)
 
 
 config_file = 'report_default.cfg'
@@ -59,13 +102,14 @@ always_added = ('.gitignore', 'font/alternate.properties',
                 r'_colored\.png$', r'\.pxm$', r'\.py$', r'.sh$', r'\.tiff$',
                 r'\.cfg$', r'(^|/).$', r'(^|/)\..$')
 
-statuses = [
-    FileStatus('Ignored', r'\.swp|\~|\/.$|\.DS_Store'),
-    FileStatus('Changed', r'^Binary files (?:\./)?(.*) and '),
-    FileStatus('Same', r'^Files (?:\./)?(.*) and '),
-    FileStatus('Missing', r'^Only in ' + other_esc + '/?(.*): (.*)'),
-    FileStatus('Added', r'^Only in \./?(.*): (.*)', always_added)
-]
+same_checker = FileStatus('Same', r'^Files (?:\./)?(.*) and ', path_from_groups=no_path)
+statuses = (
+    FileStatus('Ignored', r'\.swp|\~|\/.$|\.DS_Store', path_from_groups=whole_match),
+    same_checker,
+    ChangedFileStatus('Changed', r'^Binary files (?:\./)?(.*) and ([^\s]*)', same_checker),
+    FileStatus('Missing', r'^Only in ' + other_esc + '/?(.*): (.*)', path_from_groups=path_from_only),
+    FileStatus('Added', r'^Only in \./?(.*): (.*)', always_added, path_from_groups=path_from_only)
+)
 
 diff = subprocess.Popen(['diff', '-rs', '.', other], stdout=subprocess.PIPE)
 
