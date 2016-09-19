2e1774d016bf1643d458099d3614ebcc02d909d5
Ken Arnold <arnold@peak.im>
2016-09-18 18:06:33 -0700

    Thumbnails for pack icons

--- continuity.repack/generate_pack_image.sh
+++ continuity.repack/generate_pack_image.sh
@@ -8,3 +8,12 @@ convert \
     -background 'rgb(237,231,184)' -virtual-pixel HorizontalTile -distort Arc '415 90 280' \
     label:' continuity ' \
     override/pack.png
+
+# Every size is scaled by 8 (note third arg of Arc)
+
+convert \
+    -font Verdana -pointsize 13.75 \
+    -define distort:viewport=64x64-32-32 \
+    -background 'rgb(237,231,184)' -virtual-pixel HorizontalTile -distort Arc '415 90 35' \
+    label:' continuity ' \
+    override/pack_thumb.png
