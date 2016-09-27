fa4865111f06a56810e9982da55fa694aeaba390
Ken Arnold <arnold@peak.im>
2016-09-27 00:21:36 -0700

    exclude more on rsync

--- site/sync_site.sh
+++ site/sync_site.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
+set -e
+cd `dirname $0`
 dst_dir="site"
 if [ x"$1" == x"-f" ]; then
     dst_dir="."
 fi
-rsync -avz --delete --delete-excluded --exclude=srcs --exclude='.??*' --exclude=rsync.sh . kcrca_claritypack@ssh.phx.nearlyfreespeech.net:$dst_dir/
+rsync -avz --delete --delete-excluded --exclude=srcs --exclude='.??*' --exclude='*'.sh --exclude='?' --exclude='.?' . kcrca_claritypack@ssh.phx.nearlyfreespeech.net:$dst_dir/
