d60de0af89d0280155cdcf5bfe4ecb4e7a88ae8b
Ken Arnold <arnold@peak.im>
2016-10-10 16:30:26 -0700

    Changes at year-long level

--- site/sync_site.sh
+++ site/sync_site.sh
@@ -5,4 +5,4 @@ dst_dir="site"
 if [ x"$1" == x"-f" ]; then
     dst_dir="."
 fi
-rsync -avz --delete --delete-excluded --exclude=srcs --exclude='.??*' --exclude='*'.sh --exclude='?' --exclude='.?' . kcrca_claritypack@ssh.phx.nearlyfreespeech.net:$dst_dir/
+rsync -c -avz --delete --delete-excluded --exclude=src --exclude='.??*' --exclude='*'.sh --exclude='?' --exclude='.?' . kcrca_claritypack@ssh.phx.nearlyfreespeech.net:$dst_dir/
