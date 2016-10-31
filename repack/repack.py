8e62bef570fc9bc31a4b720a73773a005dccf862
Ken Arnold <arnold@peak.im>
2016-10-30 19:12:24 -0700

    Don't copy the variants subdir

--- repack/repack.py
+++ repack/repack.py
@@ -18,7 +18,7 @@ target_opt_re = re.compile(r'([^:]*):(.*)')
 tile_spec_re = re.compile(r'(\d+)x(\d+)(?:@(\d+),(\d+))?')
 block_id_re = re.compile(r'(\d+):?([\d&-]*)')
 ctm_opt_re = re.compile(r'([A-Z]*):?([\d:&,-]+)@?([\d]*)')
-skip_dirs_re = re.compile(r'^\.')
+skip_dirs_re = re.compile(r'^\.|^no$')
 do_not_copy_re = re.compile(r'\.(py.*|cfg|sh|pxm|config|tiff)$|/(.$|\.)')
 solid_prop_re = re.compile(r'\nsolid=(\d+)\n')
 
