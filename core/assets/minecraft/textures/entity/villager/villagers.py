3c3586474ccc8778cd8104f2ad2458b0fe8157fc
Ken Arnold <arnold@peak.im>
2017-07-08 10:03:02 -0700

    over/under layers for villagers

--- core/assets/minecraft/textures/entity/villager/villagers.py
+++ core/assets/minecraft/textures/entity/villager/villagers.py
@@ -12,6 +12,7 @@ from PIL import ImageDraw
 from PIL import ImageColor
 import numpy
 
+
 def make_sure_path_exists(path):
     try:
         os.makedirs(path)
@@ -19,15 +20,19 @@ def make_sure_path_exists(path):
         if exception.errno != errno.EEXIST:
             raise
 
+
 orig_dir = os.getcwd()
 
 professions = []
-type_imgs = []
+under_imgs = []
+over_imgs = []
 profession_imgs = []
 for file in glob.glob("parts/*.png"):
     img = Image.open(file).convert("RGBA")
-    if '/type' in file:
-        type_imgs += (img,)
+    if 'under' in file:
+        under_imgs += (img,)
+    elif 'over' in file:
+        over_imgs += (img,)
     else:
         profession_imgs += (img,)
         professions += (os.path.basename(file)[:-4],)
@@ -37,12 +42,13 @@ make_sure_path_exists(output_dir)
 for i in range(0, len(professions)):
     prof_img = profession_imgs[i]
     prof = professions[i]
-    n = 1
-    for t in type_imgs:
-        img = Image.alpha_composite(t, prof_img)
-        path = prof + ('' if n == 1 else '%d' % n) + '.png'
+    variant = 1
+    for j in range(0, len(under_imgs)):
+        img = Image.alpha_composite(under_imgs[j], prof_img)
+        img = Image.alpha_composite(img, over_imgs[j])
+        path = prof + ('' if variant == 1 else '%d' % variant) + '.png'
         img.save(os.path.join(output_dir, path))
         # Save the canonical ones
-        if i + 1 == n:
+        if i + 1 == variant:
             img.save(prof + '.png')
-        n += 1
+        variant += 1
