fb7a4cda4a77d760c59313c275968736ca9c28b7
Ken Arnold <arnold@peak.im>
2017-08-12 15:04:53 -0700

    links.sh

--- bin/links.sh
+++ bin/links.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-top=`dirname $0`
-cd $top
+bin=`dirname $0`
+cd $bin/..
 top=$PWD
 d=$top/default_resourcepack
 [ -d $d ] || (echo "No default pack: " $d && exit 1) 1>&2
@@ -22,4 +22,16 @@ for d in $top/*.repack/override; do
         o=$c/$f
         [ -d $o ] && echo ln -s $o $f/.c && ln -s $o $f/.c
     done
+done
+
+c=$top/core/assets/minecraft
+for d in $top/core/assets/*; do
+    if [[ -d $d && ! $d =~ 'minecraft' ]]; then
+	echo cd $d
+	cd $d
+	for f in `find . -type d`; do
+	    o=$c/$f
+	    [ -d $o ] && echo ln -s $o $f/.c && ln -s $o $f/.c
+	done
+    fi
 done
