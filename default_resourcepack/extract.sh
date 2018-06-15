637171f637e5e9d901f897ee5dcd7cbd4a9a857b
Ken Arnold <arnold@peak.im>
2018-06-15 12:21:01 -0700

    Update to 1.13.2pre

--- default_resourcepack/extract.sh
+++ default_resourcepack/extract.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
-unzip -qq "$@" -x '*.class' log4j2.xml META-INF/'*' mcmod.info
-rm -f pack.*
+rm -rf */
+unzip -qq "$@" -x '*.class' log4j2.xml META-INF/'*'
+#rm -f pack.*
 find */ -type f -print0 | xargs -0 chmod -w
+git add .
