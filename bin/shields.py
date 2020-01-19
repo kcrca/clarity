254e0400c68cc92649621b9035de519e2a1c3372
Ken Arnold <arnold@peak.im>
2020-01-18 18:31:33 -0800

    Derive shield patterns from banner patterns

---
+++ bin/shields.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+import glob
+
+from PIL import Image
+
+shield_scale = 16
+
+def scale(t):
+    return tuple(shield_scale * s for s in t)
+
+
+for src_file in glob.glob('entity/banner/*.png'):
+    src_img = Image.open(src_file)
+    pattern = src_img.crop((8, 8, 160, 320))
+    new_img = Image.new("RGBA", (1024, 1024), (0, 0, 0, 0))
+    new_img.paste(pattern, (32, 32))
+    new_img.save(src_file.replace('banner', 'shield'))
