1bedcee0df473abe5d2cda3cbfaea3742fe070fb
Ken Arnold <arnold@peak.im>
2020-02-07 18:15:50 -0800

    Full shield pattern

--- bin/shields.py
+++ bin/shields.py
@@ -11,7 +11,7 @@ def scale(t):
 
 for src_file in glob.glob('entity/banner/*.png'):
     src_img = Image.open(src_file)
-    pattern = src_img.crop((8, 8, 160, 320))
+    pattern = src_img.crop((8, 8, 168, 328))
     new_img = Image.new("RGBA", (1024, 1024), (0, 0, 0, 0))
     new_img.paste(pattern, (32, 32))
     new_img.save(src_file.replace('banner', 'shield'))
