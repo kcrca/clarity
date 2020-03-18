f0bca485b791bd22b78cf428947d3b68821cbe44
Ken Arnold <arnold@peak.im>
2020-03-17 22:09:40 -0700

    Remove unused "edge" in glass modles Clean background of stick item

---
+++ bin/transparify.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+
+import os
+import sys
+
+from PIL import Image
+
+for src_file in sys.argv[1:]:
+    src_img = Image.open(src_file)
+    src_data = src_img.convert('RGBA').load()
+
+    dst_img = Image.new('RGBA', src_img.size, color=None)
+    dst_data = dst_img.load()
+
+    for x in xrange(src_img.size[0]):
+        for y in xrange(src_img.size[1]):
+            data = src_data[x, y]
+            if data[3] == 0:
+                level = 0
+            else:
+                level = data[0]
+                if level < 10:
+                    level = 0
+            if data[3] not in [0,255]:
+                print data
+            dst_data[x, y] = (255, 255, 255, level)
+
+    os.rename(src_file, src_file.replace('.png', '_orig.png'))
+    dst_img.save(src_file, 'png')
