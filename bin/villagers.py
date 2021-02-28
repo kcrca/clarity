ec6c5bcdd5750a41c97da174414265997135ba80
Ken Arnold <arnold@peak.im>
2021-02-28 13:52:18 -0800

    Get the villager odds right

--- bin/villagers.py
+++ bin/villagers.py
@@ -4,7 +4,6 @@ import glob
 import os
 import re
 import shutil
-import sys
 
 from PIL import Image
 
@@ -15,9 +14,10 @@ __author__ = 'arnold'
 config = configparser.ConfigParser()
 config.read(clip.directory('config', 'villagers.cfg'))
 
-hair_re = re.compile(r'([^@]*)(?:@(\d+\.?\d*))?')
-brows_re = re.compile(r'(\d+)@(\d+),(\d+)')
-hair_color_re = re.compile(r'(\d+),(\d+)')
+odds_re = re.compile(r'([^@]*)(?:@(\d+\.?\d*))?')
+eyebrows_re = re.compile(r'(\d+)@(\d+),(\d+)')
+hair_color_pos_re = re.compile(r'(\d+),(\d+)')
+
 villager_dir = clip.directory('minecraft', 'optifine', 'random', 'entity', 'villager')
 avatar_dir = os.path.join(villager_dir)
 shutil.rmtree(avatar_dir, ignore_errors=True)
@@ -29,15 +29,12 @@ for filename in glob.glob('%s/*' % avatar_dir):
 people_config = config.items('people')
 
 style_imgs = {}
-hair_imgs = {}
-villager_imgs = {}
-career_imgs = {}
 skin_imgs = {}
 
 os.chdir(clip.directory('textures', 'entity', 'villager'))
 
-all_desc = config.get('people', 'all')
-all_hairs = all_desc.split()
+all_desc = config.get('settings', 'common_styles')
+common_styles = all_desc.split()
 
 only_listed = set()
 only_listed_desc = config.get('settings', 'only_listed')
@@ -46,168 +43,132 @@ if only_listed_desc:
 
 eyebrows = ()
 eyebrows_desc = config.get('settings', 'eyebrows')
-if eyebrows_desc:
-    for brow_desc in eyebrows_desc.split():
-        m = brows_re.match(brow_desc)
-        brow_width, x, y = m.groups()
-        eyebrows += (tuple((int(v) for v in m.groups()), ),)
+for eyebrow_desc in eyebrows_desc.split():
+    m = eyebrows_re.match(eyebrow_desc)
+    eyebrow_width, x, y = m.groups()
+    eyebrows += (tuple((int(v) for v in m.groups()), ),)
 
 no_eyebrows = set(config.get('settings', 'no_eyebrows').split())
 
 hair_color_pos = []
 hair_color_desc = config.get('settings', 'hair_color').split()
-if hair_color_desc:
-    for i in range(0, len(hair_color_desc)):
-        m = hair_color_re.match(hair_color_desc[i])
-        hair_color_pos.append(tuple(int(v) for v in m.groups()))
+for i in range(0, len(hair_color_desc)):
+    m = hair_color_pos_re.match(hair_color_desc[i])
+    hair_color_pos.append(tuple(int(v) for v in m.groups()))
 
-hat_rect_spec = re.split(r'[,\s]+', config.get('settings', 'hat'))
-hat_rect = tuple(int(x) for x in hat_rect_spec)
 
-if (len(eyebrows) > 0) != (len(hair_color_pos) > 0):
-    print("Must specify both eyebrows and hair_color pos, or neither.")
-    sys.exit(1)
-
-hair_styles = []
-hair_style_desc = config.get('settings', 'styles').split()
-if hair_style_desc:
-    for i in range(0, len(hair_style_desc)):
-        m = hair_re.match(hair_style_desc[i])
-        hair_styles.append(m.groups())
-    remaining = 100.0
-    equal_chance = 0
-    for style in hair_styles:
-        cut, chance = style
-        if chance is None:
-            equal_chance += 1
+def odds_for(descs):
+    remaining = 1.0
+    same = []
+    odds = {}
+    for desc in descs:
+        thing, percent = odds_re.match(desc).groups()
+        if percent:
+            p = float(percent) / 100
+            odds[thing] = p
+            remaining -= p
         else:
-            remaining -= float(chance)
-    for i in range(0, len(hair_styles)):
-        cut, chance = hair_styles[i]
-        if chance is None:
-            hair_styles[i] = (cut, (remaining / equal_chance))
-        else:
-            hair_styles[i] = (cut, float(chance))
+            same.append(thing)
+    for thing in same:
+        odds[thing] = remaining / len(same)
+    return odds
 
 
 def build_avatars():
     avatar_num = 2
     villager_img = Image.open('parts/villager2.png')
-    odds = {}
-    genotypes = {}
-    styles = {}
-    same_hair = {}
-    for skin, hair_descs in people_config:
-        if skin == 'all':
-            continue
-
+    genotypes = [None, None]
+    for skin_desc, hair_descs in people_config:
+        skin, _ = odds_re.match(skin_desc).groups()
         if skin in skin_imgs:
             skin_img = skin_imgs[skin]
         else:
             skin_path = 'parts/%s.png' % skin
             skin_img = skin_imgs[skin] = Image.open(skin_path).convert('RGBA')
 
-        hairs = hair_descs.split()
+        hair_for_skin = hair_descs.split()
         if skin not in only_listed:
-            hairs += all_hairs
-        for style_desc in hair_styles:
-            for hair_desc in hairs:
-                m = hair_re.match(hair_desc)
-                hair, percent = m.groups()
-                style, style_percent = style_desc
+            hair_for_skin += common_styles
+        hairs = odds_for(hair_for_skin)
+        for style in styles:
+            for hair in hairs:
                 if style == 'shaved':
+                    genotype = '%s_%s' % (skin, style)
                     hair_path = 'parts/hair_styles/hair_%s.png' % style
                 else:
+                    genotype = '%s_%s_%s' % (skin, hair, style)
                     hair_path = 'parts/hair_styles/hair_%s_%s.png' % (style, hair)
 
-                if (hair, style) in hair_imgs:
-                    hair_img = hair_imgs[(hair, style)]
-                else:
-                    hair_img = hair_imgs[(hair, style)] = Image.open(hair_path).convert('RGBA')
-                if (hair, style) in same_hair:
-                    same_hair[(hair, style)] += (avatar_num,)
-                else:
-                    same_hair[(hair, style)] = (avatar_num,)
-                if style != 'shaved':
-                    genotype = '%s_%s_%s' % (skin, hair, style)
-                else:
-                    genotype = '%s_%s' % (skin, style)
-                genotypes[avatar_num] = genotype
-                styles[avatar_num] = (style, style_percent)
-                if percent:
-                    if style != 'shaved':
-                        odds[avatar_num] = (float(percent) / 100) * (style_percent / 100) * 100
-                    else:
-                        odds[avatar_num] = style_percent
+                genotype_percent = skins[skin] * styles[style] * hairs[hair]
+                genotypes.append((genotype, genotype_percent))
+
                 img = Image.alpha_composite(villager_img, skin_img)
 
+                if (hair, style) in style_imgs:
+                    hair_img = style_imgs[(hair, style)]
+                else:
+                    hair_img = style_imgs[(hair, style)] = Image.open(hair_path).convert('RGBA')
                 # Special case for eyebrows
-                if hair not in no_eyebrows and eyebrows:
-                    hair_color = [0, 0, 0, 0]
-                    for color_pos in hair_color_pos:
-                        if hair_color[3] != 0:
-                            continue
-                        hair_color = hair_img.getpixel(color_pos)
-                    # Alpha is zero, so no real hair color
-                    if hair_color[3] != 0:
-                        eyebrow_color = clip.darker(hair_color[0:3]) + (hair_color[3],)
-                        for eyebrow in eyebrows:
-                            length, x, y = eyebrow
-                            for i in range(0, length):
-                                # Need eyebrows only if the career image hasn't set the pixel
-                                need_eyebrows = villager_img.getpixel((x, y))[3] == 0
-                                if need_eyebrows:
-                                    img.putpixel((x + i, y), eyebrow_color)
+                eyebrow_handling(hair, hair_img, img, villager_img)
                 img = Image.alpha_composite(img, hair_img)
 
                 avatar_path = '%s/villager%d.png' % (avatar_dir, avatar_num)
                 img.save(avatar_path)
                 avatar_num += 1
-                if style == 'shaved':
-                    break
 
-    # adjust odds for the number of folks with the same hair.
-    for (hair, style) in same_hair:
-        same = same_hair[(hair, style)]
-        if same[0] in odds:
-            for i in same:
-                odds[i] /= len(same)
-    return avatar_num - 1, odds, genotypes, styles
+    return genotypes
 
 
-num_avatars, odds, genotypes, styles = build_avatars()
+def eyebrow_handling(hair, hair_img, img, villager_img):
+    if hair not in no_eyebrows and eyebrows:
+        hair_color = [0, 0, 0, 0]
+        for color_pos in hair_color_pos:
+            if hair_color[3] != 0:
+                continue
+            hair_color = hair_img.getpixel(color_pos)
+        # Alpha is zero, so no real hair color
+        if hair_color[3] != 0:
+            eyebrow_color = clip.darker(hair_color[0:3]) + (hair_color[3],)
+            for eyebrow in eyebrows:
+                length, x, y = eyebrow
+                for i in range(0, length):
+                    # Need eyebrows only if the career image hasn't set the pixel
+                    need_eyebrows = villager_img.getpixel((x, y))[3] == 0
+                    if need_eyebrows:
+                        img.putpixel((x + i, y), eyebrow_color)
+
+
+styles = odds_for(config.get('settings', 'styles').split())
+skins = odds_for(k for k, _ in people_config)
+
+avatars = build_avatars()
 
 prop_path = '%s/villager.properties' % avatar_dir
 props = open(prop_path, 'w')
 
-odds_specified = sum(odds[i] for i in odds)
-
-remaining = 100.0 - odds_specified
-assert remaining > 0
-default_odds = remaining / (num_avatars - len(odds))
-weights = ''
 t = 0.0
-style_percentages = []
-for i in range(2, num_avatars + 1):
-    weight = odds[i] if i in odds else default_odds
-    if len(weights) > 0:
-        weights += ','
-    t += weight
-    # use max to make sure nothing has zero chance.
-    weights += str(max(1, int(round(weight * 1000))))
-    props.write('# %2d: %6.7f %s\n' % (i, weight, genotypes[i]))
+weights = []
+for i in range(0, len(avatars)):
+    if avatars[i]:
+        weight = avatars[i][1] * 100
+        t += weight
+        weights.append("%d" % round(weight * 100_000))
+        props.write('# %2d: %6.7f %s\n' % (i, weight, avatars[i][0]))
+    else:
+        assert i in (0, 1)
+
 
 props.write('#     %6.3f\n' % t)
 props.write('professions.1=none\n')
 props.write('textures.1=1\n')
-props.write('textures.2=1-%d\n' % num_avatars)
-props.write('weights.2=0,%s\n' % weights)
+props.write('textures.2=1-%d\n' % (len(avatars) - 2))
+props.write('weights.2=0,%s\n' % ','.join(weights))
 props.close()
 
 shutil.copytree('parts/profession', '%s/profession' % avatar_dir)
 for profession in glob.glob('parts/profession/*.png'):
     career = os.path.basename(profession)[:-5]
-    with  open('%s/profession/%s.properties' % (avatar_dir, career), 'w') as props:
+    with open('%s/profession/%s.properties' % (avatar_dir, career), 'w') as props:
         props.write('professions.1=%s\n' % career)
         props.write('textures.1=1-2\n')
         props.write('weights.1=0,100\n')
