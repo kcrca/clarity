9acbb333ae9910adc505f750b1672a4ec6300480
Ken Arnold <arnold@peak.im>
2022-03-28 21:53:32 -0700

    rehome script

---
+++ bin/rehome.sh
@@ -0,0 +1,28 @@
+#!/bin/zsh
+set -e
+set -x
+
+cd ~/clarity
+
+src=$PWD
+dst=/Applications/MultiMC.app/Data/instances/$1/.minecraft
+[[ -d $dst ]] || exec file $dst
+
+rm home
+ln -s $dst home
+cd home
+rm resourcepacks/core
+ln -s $src/core resourcepacks/
+[[ -f options.txt.bak ]] | cp options.txt options.txt.bak
+ex - options.txt << \EOF
+/^advancedItemTooltips:/s/:.*/:true
+/^autoJump:/s/:.*/:false
+/^bobView:/s/:.*/:false
+/^chatScale:/s/:.*/:0.75
+/^darkMojangStudiosBackground:/s/:.*/:true
+/^key_key.pickItem:/s/:.*/:key.keyboard.p
+/^key_key.socialInteractions:/s/:.*/:key.keyboard.unknown
+/^key_key.use:/s/:.*/:key.keyboard.left.alt
+/^soundCategory_master:/s:.*/:0.0
+/^reducedDebugInfo:/s:.*/false
+wq
