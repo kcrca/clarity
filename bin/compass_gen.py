ee6e5c8fe53769663bb0a7902894315db0dbee41
Ken Arnold <kcrca@google.com>
2022-04-10 12:21:52 -0700

    echo shard, recovery compass

--- bin/compass_gen.py
+++ bin/compass_gen.py
@@ -7,6 +7,8 @@
 __author__ = 'arnold'
 
 import json
+import os
+import shutil
 
 import clip
 
@@ -20,6 +22,10 @@ FP_SCALE = [0.3, 0.3, 0.3]
 FP_TRANSLATION = [-1, 4, 1]
 FP_X_ROT = -65
 FP_Y_ROT = 0  # Generate each override file for each angle "tick"
+
+dir = clip.directory('models')
+os.chdir(dir)
+
 overrides = []
 for i in range(0, TICKS + 1):
     day_frac = i * TICK_FRACTION
@@ -31,7 +37,7 @@ for i in range(0, TICKS + 1):
         angle_frac -= HALF_TICK_FRACTION
     overrides.append({"predicate": {"angle": angle_frac}, "model": model})
 
-    with open(clip.directory('models', json_path), 'w') as f:
+    with open(json_path, 'w') as f:
         angle = ((angle_frac + HALF_TICK_FRACTION) * 360 + 270) % 360
         json.dump({
             "parent": "item/base_compass",
@@ -50,8 +56,21 @@ for i in range(0, TICKS + 1):
         }, f, indent=4, sort_keys=True)
 
 # Now generate the primary model file that lists all the overrides generated above.
-with open(clip.directory('models', 'item', 'compass.json'), 'w') as f:
+with open('item/compass.json', 'w') as f:
     json.dump({
         "parent": "item/base_compass",
         "overrides": overrides
     }, f, indent=4, sort_keys=True)
+
+
+def toRecovery(src, dst=None):
+    old_text = open(src, 'r').read()
+    new_text = old_text.replace('compass', 'recovery_compass')
+    open(src.replace('compass', 'recovery_compass'), 'w').write(new_text)
+
+
+# Recovery compass is just modified compass
+toRecovery("item/compass.json")
+toRecovery("item/base_compass.json")
+shutil.rmtree("item/recovery_compass", ignore_errors=True)
+shutil.copytree("item/compass", "item/recovery_compass", copy_function=toRecovery)
