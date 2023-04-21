a45f02788850a8ae087886078573d5ca2d22e775
Ken Arnold <arnold@peak.im>
2023-04-20 23:28:00 -0700

    rename shard to sherd

--- bin/sherds.py
+++ bin/sherds.py
@@ -14,18 +14,18 @@ items = Path(os.path.join(clip.directory('textures', 'item')))
 parts = textures / 'parts'
 
 pot_bg = Image.open(textures / 'decorated_pot_side.png')
-shard_bg = Image.open(parts / 'shard.png')
+sherd_bg = Image.open(parts / 'sherd.png')
 
 for part in parts.glob('*.png'):
-    if part.stem == 'shard':
+    if part.stem == 'sherd':
         continue
     which = part.stem
     img = Image.open(part)
     pot_img = pot_bg.copy()
     pot_img.paste(img, mask=img)
     pot_img.save(textures / f'{which}_pottery_pattern.png')
-    masked_img = shard_bg.copy()
+    masked_img = sherd_bg.copy()
     masked_img.paste(img, (-2, 0), mask=img)
-    shard = Image.new('RGBA', img.size)
-    shard.paste(masked_img, mask=shard_bg)
-    shard.save(items / f'{which}_pottery_shard.png')
+    sherd = Image.new('RGBA', img.size)
+    sherd.paste(masked_img, mask=sherd_bg)
+    sherd.save(items / f'{which}_pottery_sherd.png')
