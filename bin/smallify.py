6de14f3126e301c5146c32c79a98ceddc6b2d8ac
Ken Arnold <arnold@peak.im>
2026-02-03 16:22:47 -0800

    small version updates

--- bin/smallify.py
+++ bin/smallify.py
@@ -4,9 +4,14 @@ import sys
 
 from PIL import Image
 
+from clip import has_transparency
+
 if len(sys.argv) <= 1:
     sys.exit(0)
 
 in_img = Image.open(sys.argv[1])
-out_img = in_img.convert('P', palette=Image.Palette.ADAPTIVE)
+if has_transparency(in_img):
+    out_img = in_img
+else:
+    out_img = in_img.convert('P', palette=Image.Palette.ADAPTIVE)
 out_img.save(sys.argv[2])
